cdcda69f423f83661e65c734b933226503c7f279
Anthony <anthony.pena0898@gmail.com>
2021-06-10 14:29:24 -0400

    Added all required and optional features

--- app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
+++ app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
@@ -1,16 +1,29 @@
 package com.codepath.android.lollipopexercise.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.palette.graphics.Palette;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.codepath.android.lollipopexercise.R;
 import com.codepath.android.lollipopexercise.models.Contact;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DetailsActivity extends AppCompatActivity {
     public static final String EXTRA_CONTACT = "EXTRA_CONTACT";
@@ -19,29 +32,184 @@ public class DetailsActivity extends AppCompatActivity {
     private TextView tvName;
     private TextView tvPhone;
     private View vPalette;
+    private FloatingActionButton fab;
+    private android.transition.Transition.TransitionListener mEnterTransitionListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
+        mEnterTransitionListener = new android.transition.Transition.TransitionListener() {
+            @Override
+            public void onTransitionStart(android.transition.Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionEnd(android.transition.Transition transition) {
+                enterReveal();
+            }
+
+            @Override
+            public void onTransitionCancel(android.transition.Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionPause(android.transition.Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionResume(android.transition.Transition transition) {
+
+            }
+        };
+        getWindow().getEnterTransition().addListener(mEnterTransitionListener);
+
         ivProfile = (ImageView) findViewById(R.id.ivProfile);
         tvName = (TextView) findViewById(R.id.tvName);
         tvPhone = (TextView) findViewById(R.id.tvPhone);
         vPalette = findViewById(R.id.vPalette);
+        fab = findViewById(R.id.fab);
+        fab.setVisibility(View.INVISIBLE);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uri = "tel:" + mContact.getNumber();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(uri));
+                startActivity(intent);
+            }
+        });
 
         // Extract contact from bundle
         mContact = (Contact)getIntent().getExtras().getSerializable(EXTRA_CONTACT);
 
         // Fill views with data
-        Glide.with(DetailsActivity.this).load(mContact.getThumbnailDrawable()).centerCrop().into(ivProfile);
         tvName.setText(mContact.getName());
         tvPhone.setText(mContact.getNumber());
+
+        // Define an asynchronous listener for image loading
+        CustomTarget<Bitmap> target = new CustomTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                // TODO 1. Instruct Glide to load the bitmap into the `holder.ivProfile` profile image view
+                Glide.with(getApplicationContext())
+                        .load(resource)
+                        .into(ivProfile);
+
+                // TODO 2. Use generate() method from the Palette API to get the vibrant color from the bitmap
+                // Set the result as the background color for `holder.vPalette` view containing the contact's name.
+                Palette.from(resource).generate(new Palette.PaletteAsyncListener() {
+                    @Override
+                    public void onGenerated(@Nullable Palette palette) {
+                        Palette.Swatch vibrant = palette.getVibrantSwatch();
+                        if(vibrant != null){
+                            vPalette.setBackgroundColor(vibrant.getRgb());
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onLoadCleared(@Nullable Drawable placeholder) {
+                // can leave empty
+            }
+        };
+
+        // TODO: Clear the bitmap and the background color in adapter
+        ivProfile.setImageBitmap(null);
+        vPalette.setBackgroundColor(0x00000000);
+
+        // Instruct Glide to load the bitmap into the asynchronous target defined above
+        Glide.with(getApplicationContext()).asBitmap().load(mContact.getThumbnailDrawable()).centerCrop().into(target);
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                exitReveal();
+                return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
+    void enterReveal() {
+        // previously invisible view
+        final View myView = findViewById(R.id.fab);
+
+        // get the center for the clipping circle
+        int cx = myView.getMeasuredWidth() / 2;
+        int cy = myView.getMeasuredHeight() / 2;
+
+        // get the final radius for the clipping circle
+        int finalRadius = Math.max(myView.getWidth(), myView.getHeight()) / 2;
+
+        // create the animator for this view (the start radius is zero)
+        Animator anim = ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0, finalRadius);
+        myView.setVisibility(View.VISIBLE);
+        anim.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                getWindow().getEnterTransition().removeListener(mEnterTransitionListener);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        // make the view visible and start the animation
+        myView.setVisibility(View.VISIBLE);
+        anim.start();
+    }
+
+    void exitReveal() {
+        // previously visible view
+        final View myView = findViewById(R.id.fab);
+
+        // get the center for the clipping circle
+        int cx = myView.getMeasuredWidth() / 2;
+        int cy = myView.getMeasuredHeight() / 2;
+
+        // get the initial radius for the clipping circle
+        int initialRadius = myView.getWidth() / 2;
+
+        // create the animation (the final radius is zero)
+        Animator anim =
+                ViewAnimationUtils.createCircularReveal(myView, cx, cy, initialRadius, 0);
+
+        // make the view invisible when the animation is done
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                myView.setVisibility(View.INVISIBLE);
+
+                supportFinishAfterTransition();
+            }
+        });
+
+        // start the animation
+        anim.start();
+    }
+
+    @Override
+    public void onBackPressed() {
+        exitReveal();
+    }
 }
--- app/src/main/java/com/codepath/android/lollipopexercise/adapters/ContactsAdapter.java
+++ app/src/main/java/com/codepath/android/lollipopexercise/adapters/ContactsAdapter.java
@@ -2,16 +2,27 @@ package com.codepath.android.lollipopexercise.adapters;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.util.Pair;
+import androidx.palette.graphics.Palette;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.codepath.android.lollipopexercise.R;
+import com.codepath.android.lollipopexercise.activities.DetailsActivity;
 import com.codepath.android.lollipopexercise.models.Contact;
 
 import java.util.List;
@@ -42,7 +53,43 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.VH> {
         Contact contact = mContacts.get(position);
         holder.rootView.setTag(contact);
         holder.tvName.setText(contact.getName());
-        Glide.with(mContext).load(contact.getThumbnailDrawable()).centerCrop().into(holder.ivProfile);
+
+
+        // Define an asynchronous listener for image loading
+        CustomTarget<Bitmap> target = new CustomTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                // TODO 1. Instruct Glide to load the bitmap into the `holder.ivProfile` profile image view
+                Glide.with(mContext)
+                        .load(resource)
+                        .into(holder.ivProfile);
+
+                // TODO 2. Use generate() method from the Palette API to get the vibrant color from the bitmap
+                // Set the result as the background color for `holder.vPalette` view containing the contact's name.
+                Palette.from(resource).generate(new Palette.PaletteAsyncListener() {
+                    @Override
+                    public void onGenerated(@Nullable Palette palette) {
+                        Palette.Swatch vibrant = palette.getVibrantSwatch();
+                        if(vibrant != null){
+                            holder.vPalette.setBackgroundColor(vibrant.getRgb());
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onLoadCleared(@Nullable Drawable placeholder) {
+                // can leave empty
+            }
+        };
+
+        // TODO: Clear the bitmap and the background color in adapter
+        holder.ivProfile.setImageBitmap(null);
+        holder.vPalette.setBackgroundColor(0x00000000);
+
+        // Instruct Glide to load the bitmap into the asynchronous target defined above
+        Glide.with(mContext).asBitmap().load(contact.getThumbnailDrawable()).centerCrop().into(target);
+
     }
 
     @Override
@@ -70,8 +117,14 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.VH> {
                 public void onClick(View v) {
                     final Contact contact = (Contact)v.getTag();
                     if (contact != null) {
-                        // Fire an intent when a contact is selected
-                        // Pass contact object in the bundle and populate details activity.
+                        Intent i = new Intent(context, DetailsActivity.class);
+                        i.putExtra(DetailsActivity.EXTRA_CONTACT, contact);
+                        Pair<View, String> p1 = Pair.create((View) ivProfile, "profile");
+                        Pair<View, String> p2 = Pair.create((View) tvName, "tvName");
+                        Pair<View, String> p3 = Pair.create(vPalette, "palette");
+                        ActivityOptionsCompat options = ActivityOptionsCompat.
+                                makeSceneTransitionAnimation(mContext, p1, p2, p3);
+                        context.startActivity(i, options.toBundle());
                     }
                 }
             });
